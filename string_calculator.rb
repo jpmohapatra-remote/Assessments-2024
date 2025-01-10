332da1f18ee9ee3e472a73b7518fb1f1bd7d2a9a
jpmohapatra-remote <jpmohapatra.remote@gmail.com>
2025-01-10 13:07:40 +0530

    Refactor add method by extracting private methods

--- string_calculator.rb
+++ string_calculator.rb
@@ -4,19 +4,32 @@
 # It provides a method to add numbers contained in a string.
 class StringCalculator
   def add(numbers)
-    raise ArgumentError, 'Input must be a string' unless numbers.is_a?(String)
+    validate_input(numbers)
     return 0 if numbers.empty?
 
-    if numbers.start_with?('//')
-      delimiter, numbers = numbers[2..].split("\n", 2)
-      nums = numbers.split(delimiter).map(&:to_i)
-    else
-      nums = numbers.gsub("\n", ',').split(',').map(&:to_i)
-    end
-
-    negatives = nums.select(&:negative?)
-    raise "Negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
+    nums = parse_numbers(numbers)
+    handle_negatives(nums)
 
     nums.sum
+  end
+
+  private
+
+  def validate_input(numbers)
+    raise ArgumentError, 'Input must be a string' unless numbers.is_a?(String)
+  end
+
+  def parse_numbers(numbers)
+    if numbers.start_with?('//')
+      delimiter, numbers = numbers[2..].split("\n", 2)
+      numbers.split(delimiter).map(&:to_i)
+    else
+      numbers.gsub("\n", ',').split(',').map(&:to_i)
+    end
+  end
+
+  def handle_negatives(nums)
+    negatives = nums.select(&:negative?)
+    raise "Negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
   end
 end

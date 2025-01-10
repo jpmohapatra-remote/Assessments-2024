60a2670afd1637ec762983bbcb399e40d34269d5
jpmohapatra-remote <jpmohapatra.remote@gmail.com>
2025-01-10 12:54:56 +0530

    Implement add method to handle empty string

---
+++ string_calculator.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# The StringCalculator class is responsible for performing calculations on strings.
+# It provides a method to add numbers contained in a string.
+class StringCalculator
+  def add(numbers)
+    0 if numbers.empty?
+  end
+end

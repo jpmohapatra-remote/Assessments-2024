b2471948531c1775f5f1d63ce106fd8bb2852455
jpmohapatra-remote <jpmohapatra.remote@gmail.com>
2025-01-10 12:56:52 +0530

    Handle two comma-separated numbers in add method

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,7 +5,8 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
+    return numbers.to_i if numbers.match(/^\d+$/)
 
-    numbers.to_i if numbers.match(/^\d+$/)
+    numbers.split(',').map(&:to_i).sum
   end
 end

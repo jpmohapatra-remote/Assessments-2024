700579c5303c8713c72c77ac511a797923241119
jpmohapatra-remote <jpmohapatra.remote@gmail.com>
2025-01-10 13:02:50 +0530

    Add spec for negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -27,5 +27,9 @@ RSpec.describe StringCalculator do
     it 'supports custom delimiters specified in the input' do
       expect(StringCalculator.new.add("//;\n1;2")).to eq(3)
     end
+
+    it 'raises an exception for negative numbers' do
+      expect { StringCalculator.new.add('1,-2,3,-4') }.to raise_error(RuntimeError, 'Negative numbers not allowed: -2, -4')
+    end
   end
 end
